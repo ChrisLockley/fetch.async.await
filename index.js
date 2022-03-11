a121f1ed7ab107df2572bb6634870d6b336cd2c4
Christian <christianlockley4@gmail.com>
2022-03-10 16:04:21 -0800

    made progress on the fetch assignment

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import fetch from 'node-fetch';
+
+async function start() {
+    try {
+    const response = await fetch('https://dog.ceo/api/breeds/list/all')
+    const data =  response.json();
+    console.log(data);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+start();
+
 // function createBreedList(breedList) {
 //     //test hey
 //     document.getElementById("breed").innerHTML = `
